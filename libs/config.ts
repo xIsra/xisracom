0918976f34a348ff2cf40845fea3abc97ae2d4fa
Isra <israelkouperman@gmail.com>
2024-08-17 15:06:15 +0300

    missing config vars

--- libs/config.ts
+++ libs/config.ts
@@ -1,9 +1,15 @@
-
 const config = {
     yt: {
         apiKey: process.env.YOUTUBE_API_KEY,
         playlistId: process.env.YOUTUBE_PLAYLIST_ID,
     }
+}
+
+if (!config.yt.apiKey) {
+    throw new Error('Missing YOUTUBE_API_KEY');
+}
+if (!config.yt.playlistId) {
+    throw new Error('Missing YOUTUBE_PLAYLIST_ID');
 }
 
 export default config;
